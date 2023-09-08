b3727cb395245682083deebd7dd79a3ad4df4b93
josueRusso <josuerusso1911@gmail.com>
2023-09-08 12:36:42 -0400

    Creando la Model

---
+++ PrimerParcial/Model/Ingresos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Model
+{
+    public class Ingresos
+    {
+        [Key]
+        public int IngresoId { get; set; }
+
+        [Required(ErrorMessage = "Debe Contener una Fecha")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = " Debe Contener un Concepto")]
+        public String? Concepto { get; set; }
+
+        [Required(ErrorMessage = " Debe Contener un Monto")]
+        public String? Monto { get; set; }
+    }
+}
